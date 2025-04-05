401b2e6b03cbd52192910d158665822b04703383
Rathin Bagchi <rathinb745@gmail.com>
2025-04-05 17:18:29 +0530

    2commit_editoption_add

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+//home rout
 app.get("/", (req, res) => {
   fs.readdir(`./files`, (err, files) => {
     res.render("index", {
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
   });
 });
 
+//read rout
 app.get("/file/:file", (req, res) => {
   fs.readFile(`./files/${req.params.file}`, "utf-8", (err, data) => {
     res.render("show", {
@@ -25,11 +27,29 @@ app.get("/file/:file", (req, res) => {
   });
 });
 
+//edit rout show
+app.get("/edit/:file", (req, res) => {
+  res.render("edit", {
+    file: req.params.file,
+  });
+});
+
+//edit rout
+app.post("/edit", (req, res) => {
+  fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}.txt`, (err) => {
+    res.redirect("/");
+  });
+});
+
+//create rout
 app.post("/create", (req, res) => {
-  fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.description, (err) => {
-      res.redirect("/")
-  })
-  
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.description,
+    (err) => {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000, () => {
